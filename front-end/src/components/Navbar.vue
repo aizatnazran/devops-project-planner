29a1f9794b99507ae3af4b43c65343d8e6af0a65
exzray2 <nazirulaiman6379@gmail.com>
2024-05-25 21:24:05 +0800

    test

--- front-end/src/components/Navbar.vue
+++ front-end/src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <nav class="main-nav">
     <router-link :to="{ name: 'Home' }">Projects</router-link>
     <router-link :to="{ name: 'Staffs' }">Staffs</router-link>
-    <router-link :to="{ name: 'Staffs' }">Dummy</router-link>
+    <router-link :to="{ name: 'Staffs' }">ComingSoon</router-link>
   </nav>
 </template>
 
